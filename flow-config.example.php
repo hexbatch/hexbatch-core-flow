5181dc74264fa40a47a953c1e5ca86fb6ccb7d2c
willwoodlief <willwoodlief@gmail.com>
2021-07-06 01:18:50 -0500

    added in constants to example config

--- flow-config.example.php
+++ flow-config.example.php
@@ -9,3 +9,10 @@ const DB_FLOW_HOSTPORT          = '3310';
 const DB_FLOW_CHARSET           = 'utf8mb4';
 const DB_FLOW_COLLATION         = 'utf8mb4_unicode_ci';
 const DB_FLOW_HOST              = 'db';
+
+
+const HEXLET_BASE_PATH = __DIR__ ;
+const HEXLET_TWIG_TEMPLATE_PATH = __DIR__ . '/views';
+const HEXLET_TWIG_PAGES_PATH = __DIR__ . '/views/pages';
+const HEXLET_TWIG_PARTIALS_PATH = __DIR__ . '/views/partials';
+const HEXLET_TWIG_CATCHE_PATH = false ;// /* set to string to enable */ __DIR__ . '/cache/twig';
